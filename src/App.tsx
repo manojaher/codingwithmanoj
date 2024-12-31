df1d1ea07863ac9c656cbff4daddb57f5dfa8384
Manoj Aher <manojaher@Manojs-Mac-mini.local>
2024-12-31 22:42:59 +0530

    Adding Blogcard to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import './App.css';
-
+import { Box, Typography } from "@mui/material";
+import { BlogCard } from "./components/Card/BlogCard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello world
-        </p>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        className="grid-container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(50px, 300px))",
+          maxWidth: "80vw",
+          minWidth: "0vh",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "40px",
+        }}
+      >
+       {
+        Array.from({length: 10}).map((_, index) => (
+          <BlogCard key={index} />
+        ))
+       }
+      </div>
     </div>
   );
 }
+
+const GridItem = () => {
+  return (
+    <Box >
+      <Typography variant="h2">Hello World</Typography>
+      <Typography variant="h6">Hello World 2</Typography>
+    </Box>
+  );
+};
 
 export default App;
---
+++ src/components/Card/BlogCard.tsx
@@ -0,0 +1,34 @@
+import { Box, Card, CardMedia, Container, Typography } from "@mui/material";
+import { CardActionArea } from "@mui/material";
+
+export const BlogCard = () => {
+  return (
+    <Card
+      className="card"
+      sx={{
+        "&:hover": {
+          boxShadow: "4px 4px 8px 4px lightgray",
+        },
+        borderRadius: "10px",
+        boxShadow: "0.5px 0.5px 8px lightgray",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image="https://cdn.pixabay.com/photo/2021/11/26/20/45/lantern-6826697_1280.jpg"
+          alt="Lantern"
+        />
+        <Box sx={{padding: "10px"}}>
+        <Typography variant="h5" fontWeight="bold" sx={{paddingBottom: "10px"}}>
+          Swift UI task Modifier
+        </Typography>
+        <Typography sx={{ variant: "body1" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos.
+        </Typography>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+};
