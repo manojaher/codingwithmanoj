8d5afecaceb3de3ef096f8c8824b65379b8c47db
Manoj Aher <manojaher@Manojs-Mac-mini.local>
2025-01-04 21:28:50 +0530

    Added dark and light themes config and used createTheme

---
+++ src/utils/theme.ts
@@ -0,0 +1,62 @@
+import { createTheme } from '@mui/material/styles';
+
+const lightPalette = {
+  primary: {
+    main: '#FFFFFF', // White
+  },
+  secondary: {
+    main: 'rgba(246, 246, 247, 1)', // Light Grey
+  },
+  tertiary: {
+    main: 'rgba(105, 106, 117, 1)', // Dark Grey
+  },
+  quaternary: {
+    main: 'rgba(75, 107, 251, 0.05)', // Light Blue
+  },
+  background: {
+    default: '#ffffff',
+    paper: '#f5f5f5',
+  },
+  text: {
+    primary: 'rgba(59, 60, 74, 1)', // Dark Blue
+    secondary: 'rgba(24, 26, 42, 1)', // Dark Blue
+    tertiary: 'rgba(75, 107, 251, 1)', // Blue
+  },
+};
+
+const darkPalette = {
+  primary: {
+    main: '#121212', // Light Blue
+  },
+  secondary: {
+    main: '#f48fb1', // Pink
+  },
+  tertiary: {
+    main: '#ffb74d', // Light Orange
+  },
+  quaternary: {
+    main: '#81c784', // Light Green
+  },
+  background: {
+    default: '#121212',
+    paper: '#424242',
+  },
+  text: {
+    primary: '#ffffff',
+    secondary: '#bbbbbb',
+  },
+};
+
+const lightTheme = createTheme({
+  palette: {
+    ...lightPalette
+  }
+});
+
+const darkTheme = createTheme({
+  palette: {
+    ...darkPalette
+  },
+});
+
+export { lightTheme, darkTheme };
