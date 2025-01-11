70722190956236c9f1bc370d7c38af0aa0e2e494
Manoj Aher <aher.manoj@outlook.com>
2025-01-11 15:30:30 +0530

    Updated husky pre-commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './routing/router';
+import './scss/styles.scss';
 
 function App() {
   return <RouterProvider router={router} />;
